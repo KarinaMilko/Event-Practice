4debad1dae6a29f299f01299fef06c89203a3ec8
KarinaMilko <milkokarina13@gmail.com>
2025-12-17 23:55:44 +0200

    Add nodemailer

--- server/index.js
+++ server/index.js
@@ -9,3 +9,37 @@ const httpServer = http.createServer(app);
 httpServer.listen(PORT, HOST, () =>
   console.log(`Server is listening http://${HOST}:${PORT}`)
 );
+
+//============================================================
+const nodemailer = require('nodemailer');
+
+(async () => {
+  try {
+    const account = await nodemailer.createTestAccount();
+
+    const transporter = nodemailer.createTransport({
+      host: account.smtp.host,
+      port: account.smtp.port,
+      secure: account.smtp.secure,
+      auth: {
+        user: account.user,
+        pass: account.pass,
+      },
+    });
+
+    let message = {
+      from: 'Event Calendar <no-reply@events.gmail.com>',
+      to: 'Recipient <recipient@example.com>',
+      subject: 'Congrats',
+      text: 'Happy birthday to you!',
+      html: '<p><b>Happy birthday</b> to you!</p>',
+    };
+
+    const info = await transporter.sendMail(message);
+    console.log('Message sent: %s', info.messageId);
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  } catch (error) {
+    console.log(error);
+  }
+})();
