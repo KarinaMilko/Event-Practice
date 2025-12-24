38737097e852759875dd2f0ccb2ef2e5aacd1ada
KarinaMilko <milkokarina13@gmail.com>
2025-12-25 00:13:34 +0200

    Add send male on users birthday

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const app = require('./app');
+const initJobs = require('./jobs');
 
 const PORT = process.env.PORT ?? 5000;
 const HOST = process.env.HOST ?? 'localhost';
@@ -10,36 +11,4 @@ httpServer.listen(PORT, HOST, () =>
   console.log(`Server is listening http://${HOST}:${PORT}`)
 );
 
-//============================================================
-const nodemailer = require('nodemailer');
-
-(async () => {
-  try {
-    const account = await nodemailer.createTestAccount();
-
-    const transporter = nodemailer.createTransport({
-      host: account.smtp.host,
-      port: account.smtp.port,
-      secure: account.smtp.secure,
-      auth: {
-        user: account.user,
-        pass: account.pass,
-      },
-    });
-
-    let message = {
-      from: 'Event Calendar <no-reply@events.gmail.com>',
-      to: 'Recipient <recipient@example.com>',
-      subject: 'Congrats',
-      text: 'Happy birthday to you!',
-      html: '<p><b>Happy birthday</b> to you!</p>',
-    };
-
-    const info = await transporter.sendMail(message);
-    console.log('Message sent: %s', info.messageId);
-    // Preview only available when sending through an Ethereal account
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-  } catch (error) {
-    console.log(error);
-  }
-})();
+initJobs();
