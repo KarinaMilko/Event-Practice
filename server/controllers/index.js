16200852267401deca40a7d66606f07da2a96c5e
KarinaMilko <milkokarina13@gmail.com>
2026-01-05 00:19:26 +0200

    Add getCategories controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,3 +1,5 @@
 module.exports.eventsController = require('./eventsController');
 
 module.exports.creatorEventsController = require('./creatorEventsController');
+
+module.exports.categoriesController = require('./categoriesController');
