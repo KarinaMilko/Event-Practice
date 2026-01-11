85a15a8473c6079ba9b560614851cef2ecd59c3d
KarinaMilko <milkokarina13@gmail.com>
2026-01-11 23:04:19 +0200

    Add category filter

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,7 +5,13 @@ const httpClient = axios.create({ baseURL: 'http://localhost:5000/api' });
 
 export const getEvents = filter =>
   httpClient.get(
-    `/events?${queryString.stringify(filter, { skipEmptyString: true })}`
+    `/events?${queryString.stringify(filter, {
+      skipEmptyString: true,
+      arrayFormat: 'comma',
+    })}`
   );
 
 //   httpClient.get(`/events?isOnline=${filter.isOnline}`);
+//   httpClient.get(`/events?isOnline=${filter.isOnline}&categoryId=${filter.categoryId}`);
+
+export const getCategories = () => httpClient.get('/categories');
--- client/src/pages/EventsPage/index.jsx
+++ client/src/pages/EventsPage/index.jsx
@@ -1,36 +1,65 @@
 import { connect } from 'react-redux';
-import { getEventsThunk, setFilter } from '../../store/slices/eventsSlice';
+import {
+  changeCategoriesFilter,
+  getEventsThunk,
+  setFilter,
+} from '../../store/slices/eventsSlice';
 import { useEffect } from 'react';
+import { getCategoriesThunk } from '../../store/slices/categoriesSlice';
 
 function EventsPage({
   getEvents,
+  getCategories,
+  changeCategoriesFilter,
   events,
   filter,
   setFilter,
   isFetching,
   errors,
+  categories,
 }) {
+  useEffect(() => {
+    getCategories();
+  }, []);
+
   useEffect(() => {
     getEvents(filter);
   }, [filter]);
 
-  const { isOnline } = filter;
+  const { isOnline, categoryId: selectedCategories } = filter;
 
   return (
     <>
       <section>
         <h2>Filters</h2>
-        <select
-          value={isOnline}
-          onChange={e => {
-            setFilter({ isOnline: e.target.value });
-          }}
-        >
-          <option value="">All</option>
-          <option value={true}>Online</option>
-          <option value={false}>Offline</option>
-        </select>
+        <section>
+          <h3>Online/Offline:</h3>
+          <select
+            value={isOnline}
+            onChange={e => {
+              setFilter({ isOnline: e.target.value });
+            }}
+          >
+            <option value="">All</option>
+            <option value={true}>Online</option>
+            <option value={false}>Offline</option>
+          </select>
+        </section>
+        <section>
+          <h3>Categories:</h3>
+          {categories.map(c => (
+            <label key={c.id}>
+              <input
+                type="checkbox"
+                checked={selectedCategories.includes(c.id)}
+                onChange={() => changeCategoriesFilter(c.id)}
+              />
+              {c.name}
+            </label>
+          ))}
+        </section>
       </section>
+
       <ul>
         {events.map(e => (
           <li key={e.id} style={{ marginTop: '15px' }}>
@@ -42,11 +71,16 @@ function EventsPage({
   );
 }
 
-const mapStateToProps = ({ eventsData }) => eventsData;
+const mapStateToProps = ({ eventsData, categoriesData: { categories } }) => ({
+  ...eventsData,
+  categories,
+});
 
 const mapDispatchToProps = dispatch => ({
   getEvents: filter => dispatch(getEventsThunk(filter)),
+  getCategories: () => dispatch(getCategoriesThunk()),
   setFilter: newFilter => dispatch(setFilter(newFilter)),
+  changeCategoriesFilter: id => dispatch(changeCategoriesFilter(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventsPage);
